ec35d5c63e92f783f342fbfab3dced5163b37e9b
gemmamary <gemmamarymcgee@gmail.com>
2019-01-16 10:43:27 +0000

    Update CharacterMatrix.java

--- CharacterMatrix.java
+++ CharacterMatrix.java
@@ -72,42 +72,43 @@ public class CharacterMatrix {
 
     public static String getNewConcatenatedCharacters(int size, String letters) {
 
-    int numberOfChars = size * size;
+        int numberOfChars = size * size;
 
-    char[] matrix = new char[size];
-    ArrayList matrixList = new ArrayList(); 
-    String oddNumbers = "";
+        char[] matrix = new char[size];
+        ArrayList matrixList = new ArrayList(); 
+        String oddNumbers = "";
 
-    for (int j = 0; j < size; j++) {
-        for (int i = 0; i < matrix.length; i++) {
-            int[] indexes = getRandomNumbersForIndexPositions(size);
-            char letter = letters.charAt(indexes[i]);
-            matrix[i] = letter;
-            matrixList.add(matrix[i]);
-            System.out.print(matrix[i] + " ");	
-        }	
-    System.out.println();
+        for (int j = 0; j < size; j++) {
+            for (int i = 0; i < matrix.length; i++) {
+                int[] indexes = getRandomNumbersForIndexPositions(size);
+                char letter = letters.charAt(indexes[i]);
+                matrix[i] = letter;
+                matrixList.add(matrix[i]);
+                System.out.print(matrix[i] + " ");	
+            }	
+        System.out.println();
+        }
+
+        int len = matrixList.size();
+
+        for (int k = 0; k < len; k++) {
+            if (k % 2 == 0) {
+                oddNumbers += matrixList.get(k);
+            }
+            k++;		
+        }
+
+        int half = (size + 1) / 2;
+        String[] odds = oddNumbers.split("(?<=\\G.{" + half + "})");
+        String gotcha = "";
+
+        for (int l = 0; l < odds.length; l++) {
+            if (l % 2 == 0) {
+                gotcha += odds[l];
+            }
+        }
+        return "Odd letters from every odd row: " + gotcha;		 		
     }
-
-    int len = matrixList.size();
-
-    for (int k = 0; k < len; k++) {
-if (k % 2 == 0) {
-oddNumbers += matrixList.get(k);
-}
-k++;		
-}
-
-int half = (size + 1) / 2;
-String[] odds = oddNumbers.split("(?<=\\G.{" + half + "})");
-String gotcha = "";
-
-for (int l = 0; l < odds.length; l++) {
-if (l % 2 == 0) {
-gotcha += odds[l];
-}
-}
-return "Odd letters from every odd row: " + gotcha;		 		
 }
 
 // request user to enter number between 5 and 10 (x)
@@ -122,5 +123,4 @@ return "Odd letters from every odd row: " + gotcha;
 // return concat characters from every odd cell from every odd row in matrix
 // assuming each line has an id find every odd numbered line
 // push the odd characters in each line to a new array and return
-}
 
